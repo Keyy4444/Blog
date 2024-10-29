b2a3b6fbd2ad30b220db6593b724a4a6b405fd6e
Oleksii <oleksprokopchenko@gmail.com>
2024-10-29 05:06:37 +0200

    configs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allows any hostname
+        port: "", // No specific port
+        pathname: "/**", // Allows any path
+      },
+    ],
+  },
 };
 
 export default nextConfig;
